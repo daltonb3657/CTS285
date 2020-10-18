c7c91a122a7f31fbc0e948f58e28e0d0bc210ae0
daltonb3657 <daltonb3657@student.faytechcc.edu>
2020-10-18 18:46:58 -0400

    Added menu selection

---
+++ WordChainGame/WordChainLib/StandardMessages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordChainLib
+{
+    public class StandardMessages
+    {
+        public static string DisplayWelcomeMessage()
+        {
+            return "Welcome to the word chain game";
+        }
+        public static string DisplayMenu()
+        {
+            return "1. Play Game" +
+                "\n2. High Scores" +
+                "\n3. Exit" +
+                "\n--> ";
+        }
+        public static string DisplayMenuError()
+        {
+            return "Invalid input! Enter a number from the list.";
+        }
+        public static string GetWord()
+        {
+            return "Enter your word: ";
+        }
+        public static string DisplayWordError()
+        {
+            return "The word you entered does not start with the previously entered words last letter.";
+        }
+        public static string DisplayWordReusedErroer()
+        {
+            return "The word you entered has already been used";
+        }
+        public static string DisplayYouLose(int score)
+        {
+            return $"You have lost." +
+                $"\nYour score is - {score}";
+        }
+    }
+}
